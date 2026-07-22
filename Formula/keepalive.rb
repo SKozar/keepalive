92a9bfd8ca3457ccf4e98c5420d3b9e8d0ee5c2f
Kozar Sviatoslav <slav@kozar.xyz>
2026-07-22 13:52:29 -0600

    v0.3.0: update formula

--- Formula/keepalive.rb
+++ Formula/keepalive.rb
@@ -1,9 +1,9 @@
 class Keepalive < Formula
   desc "Keep macOS awake for Teams during chosen hours"
   homepage "https://github.com/skozar/keepalive"
-  version "0.2.1"
+  version "0.3.0"
   url "https://github.com/skozar/keepalive/releases/download/v#{version}/keepalive"
-  sha256 "5d743d9dd1df6a2df92fa9abf28649922c4dcbb92271b8a07d7018b52ca6c334"
+  sha256 "e85d36f6540fbbc02f3d52275e92de18b6f05d69d1c92f0f24c7f15b9102a498"
 
   def install
     bin.install "keepalive"
